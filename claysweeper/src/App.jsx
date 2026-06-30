5ce249b92e6f9a39991497dcf369701f534e5f1c
Barrcamps <camposjuan881@gmail.com>
2026-06-30 19:29:05 -0300

    First commit. Git init.

---
+++ claysweeper/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import ClayBoard from "./components/Clay-board";
+
+function App() {
+  return (
+    <>
+      <ClayBoard />
+    </>
+  );
+}
+
+export default App;
---
+++ claysweeper/src/components/Clay-board.jsx
@@ -0,0 +1,55 @@
+import "../styles/ClayBoard.css";
+
+const ClayBoard = () => {
+  return (
+    <div className="board">
+      <div className="top-section">
+        <p>Tiempo: 00:00</p>
+        <div className="btn-container">
+          <button type="button" className="btn-reinicio">
+            😀
+          </button>
+        </div>
+        <p>Minas: 20</p>
+      </div>
+      <div className="celdas">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis ipsum
+          delectus numquam similique eius cupiditate accusamus consectetur quod,
+          ratione officia in at vero illo laudantium libero ipsam odit natus
+          dolore. Voluptates, inventore reprehenderit. Id adipisci perspiciatis
+          ratione corrupti earum nulla, hic dolore animi facilis. Libero sit
+          rerum ipsum molestiae mollitia dolores distinctio atque harum labore
+          amet ipsam iste, obcaecati praesentium. Qui deserunt libero
+          dignissimos, tempora placeat vero minus nulla vitae sapiente, dolore
+          ipsam rerum natus dolor veritatis consequuntur totam fugiat id minima
+          dolorum error tempore aspernatur! Voluptates hic voluptatem delectus?
+          Commodi accusantium nemo id possimus incidunt quibusdam labore ducimus
+          reprehenderit odit sed. A consequatur delectus harum necessitatibus
+          maiores similique error, cumque suscipit soluta sint, eligendi
+          incidunt, fugiat eaque ipsum adipisci. Praesentium nemo, dolorum
+          reiciendis, temporibus commodi voluptas assumenda ut sit voluptatum
+          modi, quo maxime rem itaque odio! Sunt accusamus fuga earum aut
+          asperiores accusantium exercitationem soluta. Aut et deleniti aliquid.
+          Corrupti sint laborum quasi voluptate nesciunt expedita ducimus
+          laudantium deserunt placeat cupiditate enim nobis perspiciatis,
+          consectetur animi minima rem consequuntur ut doloribus repudiandae
+          sequi debitis. Molestiae laboriosam maxime facere distinctio.
+          Accusamus consequatur omnis officia rem odit iste excepturi beatae id
+          eum cumque impedit corrupti, doloremque dolor autem sed. Obcaecati,
+          exercitationem? Quidem quibusdam obcaecati accusamus doloribus nostrum
+          voluptas pariatur blanditiis facilis. Maxime officia culpa libero
+          maiores doloremque, rem quas a mollitia vel saepe hic laborum pariatur
+          fuga dolorem quibusdam assumenda soluta, ipsum alias quia repellat.
+          Aliquam vitae molestiae velit unde exercitationem. Rerum natus
+          voluptatem dolorum cupiditate optio ad laborum temporibus accusantium
+          illum non doloribus, iure suscipit! Delectus, accusantium et quas
+          omnis nisi dignissimos, quasi quibusdam fugit tenetur officia vero
+          dolorum voluptatum. Quae beatae sequi voluptas pariatur.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ClayBoard;
