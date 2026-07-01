a0004af3dd38f560d33d66a92f3363b24245817d
Barrcamps <camposjuan881@gmail.com>
2026-06-30 21:52:35 -0300

    First commit in develop branch

--- claysweeper/src/components/Clay-board.jsx
+++ claysweeper/src/components/Clay-board.jsx
@@ -14,38 +14,38 @@ const ClayBoard = () => {
       </div>
       <div className="celdas">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis ipsum
-          delectus numquam similique eius cupiditate accusamus consectetur quod,
-          ratione officia in at vero illo laudantium libero ipsam odit natus
-          dolore. Voluptates, inventore reprehenderit. Id adipisci perspiciatis
-          ratione corrupti earum nulla, hic dolore animi facilis. Libero sit
-          rerum ipsum molestiae mollitia dolores distinctio atque harum labore
-          amet ipsam iste, obcaecati praesentium. Qui deserunt libero
-          dignissimos, tempora placeat vero minus nulla vitae sapiente, dolore
-          ipsam rerum natus dolor veritatis consequuntur totam fugiat id minima
-          dolorum error tempore aspernatur! Voluptates hic voluptatem delectus?
-          Commodi accusantium nemo id possimus incidunt quibusdam labore ducimus
-          reprehenderit odit sed. A consequatur delectus harum necessitatibus
-          maiores similique error, cumque suscipit soluta sint, eligendi
-          incidunt, fugiat eaque ipsum adipisci. Praesentium nemo, dolorum
-          reiciendis, temporibus commodi voluptas assumenda ut sit voluptatum
-          modi, quo maxime rem itaque odio! Sunt accusamus fuga earum aut
-          asperiores accusantium exercitationem soluta. Aut et deleniti aliquid.
-          Corrupti sint laborum quasi voluptate nesciunt expedita ducimus
-          laudantium deserunt placeat cupiditate enim nobis perspiciatis,
-          consectetur animi minima rem consequuntur ut doloribus repudiandae
-          sequi debitis. Molestiae laboriosam maxime facere distinctio.
-          Accusamus consequatur omnis officia rem odit iste excepturi beatae id
-          eum cumque impedit corrupti, doloremque dolor autem sed. Obcaecati,
-          exercitationem? Quidem quibusdam obcaecati accusamus doloribus nostrum
-          voluptas pariatur blanditiis facilis. Maxime officia culpa libero
-          maiores doloremque, rem quas a mollitia vel saepe hic laborum pariatur
-          fuga dolorem quibusdam assumenda soluta, ipsum alias quia repellat.
-          Aliquam vitae molestiae velit unde exercitationem. Rerum natus
-          voluptatem dolorum cupiditate optio ad laborum temporibus accusantium
-          illum non doloribus, iure suscipit! Delectus, accusantium et quas
-          omnis nisi dignissimos, quasi quibusdam fugit tenetur officia vero
-          dolorum voluptatum. Quae beatae sequi voluptas pariatur.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+          omnis deleniti. Laudantium enim voluptates officiis voluptas quaerat
+          laborum, sint obcaecati, atque veniam recusandae ratione quibusdam
+          consequuntur minima vel ex exercitationem. Magnam repellat cumque,
+          ullam cum nostrum, ratione vel perferendis, earum omnis obcaecati
+          blanditiis exercitationem consectetur vitae corporis sapiente
+          repudiandae! Quidem dolorem facilis soluta rem laboriosam atque
+          provident ullam? Totam, iste. Repellendus alias vitae, amet ab
+          voluptate sed odit ratione nobis quae labore quos reiciendis
+          voluptatibus debitis dolore perferendis possimus facilis fugit
+          eveniet. Rerum libero quos eos quo animi nihil nesciunt! Non,
+          molestias pariatur? Perferendis iure odio explicabo ut deserunt quidem
+          deleniti soluta assumenda? Delectus eos cupiditate distinctio
+          molestiae sit nostrum animi magnam ducimus, alias dignissimos esse
+          sapiente quas reiciendis autem? Quae incidunt est asperiores debitis
+          dolorem rerum, placeat necessitatibus reiciendis animi dicta autem
+          excepturi fugiat doloribus voluptate corporis sequi explicabo nihil
+          ducimus perspiciatis? Ea provident eligendi illum odio, repellendus
+          placeat! Incidunt quibusdam beatae repellat ullam modi molestiae
+          cupiditate ad enim dolor. Non assumenda nam nostrum eius quia fugit
+          veniam iusto voluptatem optio sequi aspernatur, a, modi accusamus,
+          maxime maiores? Soluta. Neque quaerat, doloremque voluptas nihil
+          voluptates officiis veritatis quia earum quas enim aliquam labore?
+          Laboriosam laborum porro accusantium hic animi similique esse labore,
+          in non nulla id, quis pariatur doloremque. Odit, nisi perspiciatis
+          magnam iste minus cupiditate unde tempore in quo laborum ipsum.
+          Necessitatibus dolorem dolores obcaecati, voluptas corrupti minus
+          nostrum quo ab, aliquam excepturi illum quidem cumque aliquid
+          quisquam! Ex optio mollitia obcaecati odio, similique reprehenderit
+          natus voluptas provident error, minus velit maiores quis amet atque
+          eligendi quia deserunt quasi. Culpa commodi quis, dicta odio enim hic?
+          Rem, delectus!
         </p>
       </div>
     </div>
